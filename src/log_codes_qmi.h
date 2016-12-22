45a5f837f4fd3fbbb0c0a406ba6f6559b14dafb6
Harald Welte <laforge@gnumonks.org>
2016-12-22 10:45:41 +0100

    Enable QMUX logging

---
+++ src/log_codes_qmi.h
@@ -0,0 +1,8 @@
+
+#define LOG_QMI_RESERVED_CODES_BASE_C           0x38E
+#define LOG_QMI_LAST_C				(32 + LOG_QMI_RESERVED_CODES_BASE_C)
+#define LOG_QMI_CALL_FLOW_C                     0x4CF
+#define LOG_QMI_SUPPORTED_INTERFACES_C          0x588
+#define LOG_QMI_RESERVED_NEW_CODES_BASE_C       0x80B
+#define LOG_QMI_NEW_LAST_C                      (32 + LOG_QMI_RESERVED_CODES_BASE_C)
+

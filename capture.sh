a3ffa99436375a23139c265a77ab269d4d3adbf3
Eric Wild <ewild@sysmocom.de>
2020-05-12 18:26:10 +0200

    update capture script

--- capture.sh
+++ capture.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
-current_time=$(date "+%Y.%m.%d_%H.%M.%S")
-filename="capture_"$current_time".pcap"
+#!/bin/bash
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -s|--serialpath) serialpath="$2"; shift ;;
+        -f|--filename) filename="$2"; shift ;;
+        -Q|--qcdebug) qcdebug=1 ;;
+        *) echo "unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+done
 
 killall tcpdump
 tcpdump -i any udp port 4729 -w $filename &
 tcpdump_pid=$!
-./src/osmo-qcdiag-log $*
+./src/osmo-qcdiag-log -s $serialpath -G
 kill -9 $tcpdump_pid
-
